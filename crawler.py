0a9ca260c874d83e1edf42b9a4944eeb1e21499d
Frédéric Sicot <frederic.sicot@airbus.com>
2021-10-19 11:24:18 +0200

    add licence and crawler module

---
+++ crawler.py
@@ -0,0 +1,31 @@
+import gitlab
+from gitlab.exceptions import GitlabParsingError
+
+
+TAG = "inner-source"
+# private token or personal token authentication
+gl = gitlab.Gitlab.from_config('gitlab', ['/Users/sicot_f/.python-gitlab.cfg'])
+
+# list all the projects
+# manual iterations over the generator as to capture the GitlabParsingError exception
+items = gl.projects.list(as_list=False, owned=True)
+while True:
+    try:
+        repo = items.next()
+        attributes = repo.attributes
+        if TAG in attributes["tag_list"]:
+            print(attributes)
+    except GitlabParsingError:
+        continue
+    except StopIteration:
+        break
+
+# for item in items:
+#     try:
+#         attributes = item.attributes
+#         if TAG in attributes["tag_list"]:
+#             print(attributes)
+#     except gitlab.exceptions.GitlabParsingErro:
+#         continue
+#
+#
